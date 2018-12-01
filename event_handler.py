1f0a34c7e44aa0c794ee9eb28527a13f444c8c9b
Grisha Leikin <imidg3001@yandex.ru>
2018-12-01 19:44:09 +0300

    code refactoring

--- event_handler.py
+++ event_handler.py
@@ -1,17 +1,25 @@
-dange_level = {
-    'NORMAL': 0,
-    'WARNING': 1,
-    'CRITICAL': 2,
-}
+import json
 
-all_event = {
-    'HUMAN': 1,
-    'SMOKE': 2
-}
+dange_level = {}
 
-bad_event = {
-    'FIRE': {'CODE': all_event['HUMAN'] | all_event['SMOKE'], 'LEVEL': dange_level['CRITICAL']},
-}
+all_event = {}
+
+bad_event = {}
+
+
+def load_handle_event_scripts():
+    global bad_event
+    global all_event
+    global dange_level
+    bad_event = json.load(open('bad_event.json', 'r'))
+    all_event = json.load(open('all_event.json', 'r'))
+    dange_level = json.load(open('dange_level.json', 'r'))
+
+
+def save_handle_event_scripts():
+    open('dange_level.json', 'w').write(json.dumps(dange_level))
+    open('all_event.json', 'w').write(json.dumps(all_event))
+    open('bad_event.json', 'w').write(json.dumps(bad_event))
 
 
 def handle_event(event_code):
